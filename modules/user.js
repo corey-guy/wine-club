546c0610c2f357f484b1b59d7c4b80f38cd754c1
corey-guy <corey.drees@gmail.com>
2020-11-30 23:46:36 -0500

    created custom registration page

---
+++ modules/user.js
@@ -0,0 +1,12 @@
+export default class User {
+
+	constructor(username, email, password) {
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+
+	getUsername() {
+		return this.username;
+	}
+}
